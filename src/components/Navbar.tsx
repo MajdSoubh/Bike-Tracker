4909179334ae91db131c17814f964d97af21217f
MajdSoubh <majdsoubh53@gmail.com>
2025-01-02 14:28:50 +0300

    Add: navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { NavLink } from "react-router";
+
+const Navbar: React.FC = () => {
+  return (
+    <div className="bg-black px-20 py-4">
+      <div className="text-white flex justify-end items-center gap-4 ">
+        <NavLink
+          to="/"
+          className="cursor-pointer text-slate-200 hover:text-white transition-all aria-[current=page]:text-white relative after:absolute after:left-1/2 after:-bottom-1 after:w-0 aria-[current=page]:after:w-[30px] after:-translate-x-1/2 after:h-[2px] after:bg-blue-200 after:transition-all"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/help"
+          className="cursor-pointer text-slate-200 hover:text-white transition-all aria-[current=page]:text-white relative after:absolute after:left-1/2 after:-bottom-1 after:w-0 aria-[current=page]:after:w-[30px] after:-translate-x-1/2 after:h-[2px] after:bg-blue-200 after:transition-all"
+        >
+          Help
+        </NavLink>
+        <NavLink
+          to="/about"
+          className="cursor-pointer text-slate-200 hover:text-white transition-all aria-[current=page]:text-white relative after:absolute after:left-1/2 after:-bottom-1 after:w-0 aria-[current=page]:after:w-[30px] after:-translate-x-1/2 after:h-[2px] after:bg-blue-200 after:transition-all"
+        >
+          About
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
