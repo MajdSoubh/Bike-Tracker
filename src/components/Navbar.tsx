d19e6e7253910cfd4ffb2d5c4f90a0ae38c63655
MajdSoubh <majdsoubh53@gmail.com>
2025-01-03 19:20:20 +0300

    Style: decrease the padding size in small screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="bg-black px-20 py-4">
-      <div className="text-white flex justify-end items-center gap-4 ">
+    <div className="bg-black px-8 md:px-20  py-4">
+      <div className="text-white flex justify-start items-center gap-4 ">
         <NavLink
           to="/"
           className="cursor-pointer text-slate-200 hover:text-white transition-all aria-[current=page]:text-white relative after:absolute after:left-1/2 after:-bottom-1 after:w-0 aria-[current=page]:after:w-[30px] after:-translate-x-1/2 after:h-[2px] after:bg-blue-200 after:transition-all"
